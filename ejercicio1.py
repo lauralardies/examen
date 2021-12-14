b4854862129ae07d28df5ba3e4c42f4764ec091e
lauralardies <lrodrlar@myuax.com>
2021-12-14 12:10:00 +0100

    cambios

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,23 +1,4 @@
-banana = list("Banana")
-banana1 = []
-banana2 = []
-
-def sepVowelConsonant(banana, banana1, banana2):
-    banana1 = []
-    banana2 = []
-    for i in range(len(banana)):
-        if banana[i] == "a" or banana[i] == "e" or banana[i] == "i" or banana[i] == "o" or banana[i] == "u":
-            banana[i] = "Vowel"
-        else:
-            banana[i] = "Consonant"
-
-    for i in range(len(banana)):
-        if banana[i] == "Vowel":
-            banana1.append(banana[i])
-        if banana[i] == "Consonant":
-            banana2.append(banana[i])
-
-# Player 1 --> Plays with vowels (banana1)
+# Player 1 --> Plays with vowels
 
 def pyramid(banana):
     for i in range(len(banana) + 1):
@@ -31,18 +12,7 @@ def printPyramid(banana):
         solution = solution + banana[i] + " "
     return solution
 
-# Player 2 --> Plays with consonants (banana2)
-
-def pyramid2(banana):
-    for i in range(len(banana) + 1):
-        line = " "*(len(banana) - i) + printPyramid2(banana[0:i])
-        print(line)
-
-def printPyramid2(banana):
-    solution = ""
-    for i in range(len(banana)):
-        solution = solution + banana[i] + " "
-    return solution
+# Player 2 --> Plays with consonants
 
 def points(banana):
     stuart_points = 0
@@ -61,6 +31,6 @@ def points(banana):
     else:
         print ("Draw")
 
+banana = list(input("Choose your string of the word banana: "))
 pyramid(banana)
-pyramid2(banana)
 points(banana)
